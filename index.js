dcb5fc4b55ed52f7dbf4e0d1fe281465be4bf5f5
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-06 05:20:01 +0900

    feat: 연산기능을 수행할 calculate 객체 임시 생성

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-document
-  .querySelector(".js-result-pointer_btn")
-  .addEventListener("click", () => {
-    const firstValue = document.querySelector(".js-firstValue").value;
-    const secondValue = document.querySelector(".js-secondValue").value;
+const btn = document.querySelectorAll(".js-result-pointer_btn");
+const a = document.querySelectorAll(".js-firstValue").value;
+const b = document.querySelectorAll(".js-secondValue").value;
 
-    if (firstValue) {
-      if (secondValue) {
-        document.querySelector(".js-result").textContent =
-          firstValue * secondValue;
-        document.querySelector(".js-errorMessage").textContent = "";
-      } else {
-        document.querySelector(".js-errorMessage").textContent =
-          "Please enter the second value";
-      }
-    } else {
-      document.querySelector(".js-errorMessage").textContent =
-        "Please enter the first value";
-    }
-  });
+const calculate = {
+  add: {
+    name: "add",
+    symbol: "+",
+    calculus: function () {
+      return a + b;
+    },
+  },
+  sub: {
+    name: "sub",
+    symbol: "-",
+    calculus: function () {
+      return a - b;
+    },
+  },
+  mul: {
+    name: "mul",
+    symbol: "X",
+    calculus: function () {
+      return a * b;
+    },
+  },
+  div: {
+    name: "div",
+    symbol: "¡À",
+    calculus: function () {
+      return a / b;
+    },
+  },
+};
+
+// function isNum(/* object */) {
+//   if (/* !firstValue && !secondValue */ ){
+//     object.errorMessage.textContent ="Please enter the empty value";
+//   }
+// }
+
+function init() {
+  btn.addEventListener("click");
+}
+
+init();
