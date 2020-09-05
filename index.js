5f4f9c8bbaad16ac001bdfe8864d58c5b9f82f87
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-06 08:55:45 +0900

    feat: 함수 2개 추가
    - 클릭된 계산 버튼이 있는 박스의 연산자를 찾는 함수 추가
    - 찾은 연산자로 해당 박스의 숫자 2개를 찾는 함수 추가

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const btn = document.querySelectorAll(".js-result-pointer_btn");
-const a = document.querySelectorAll(".js-firstValue").value;
-const b = document.querySelectorAll(".js-secondValue").value;
-
 const calculate = {
   add: {
-    name: "add",
-    symbol: "+",
+    className: "js-add",
+    operator: "+",
     calculus: function () {
       return a + b;
     },
   },
   sub: {
-    name: "sub",
-    symbol: "-",
+    className: "js-sub",
+    operator: "-",
     calculus: function () {
       return a - b;
     },
   },
   mul: {
-    name: "mul",
-    symbol: "X",
+    className: "js-mul",
+    operator: "X",
     calculus: function () {
       return a * b;
     },
   },
   div: {
-    name: "div",
-    symbol: "÷",
+    className: "js-div",
+    operator: "÷",
     calculus: function () {
       return a / b;
     },
   },
 };
+// const operators = Object.keys(calculate);
+let firstValue = 0;
+let secondValue = 0;
 
 // function isNum(/* object */) {
 //   if (/* !firstValue && !secondValue */ ){
@@ -39,8 +39,23 @@ const calculate = {
 //   }
 // }
 
+function setNumbers(currentOperatorClassName) {
+  let currentOperator = document.getElementsByClassName(
+    currentOperatorClassName
+  );
+  firstValue = currentOperator[0].previousElementSibling.value;
+  secondValue = currentOperator[0].nextElementSibling.value;
+}
+
+function findOperator(e) {
+  const currentBtnClassList = e.target.classList;
+  let currentOperatorClassName =
+    currentBtnClassList[currentBtnClassList.length - 1]; // 클릭된 버튼의 클래스 배열중에 마지막 요소가 연산자를 나타내는 클래스
+  setNumbers(currentOperatorClassName);
+}
+
 function init() {
-  btn.addEventListener("click");
+  btn.forEach(() => addEventListener("click", findOperator));
 }
 
 init();
