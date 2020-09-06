6a845eebb83cf97d1b974759f5525a835f5d0509
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-07 02:42:38 +0900

    feat: 에러메세지 위치를 특정하기 위해 에러메세지에도 keyClass 추가

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 /*
-같은 계산기 박스 부모를 가지는 연산기호, 화살표버튼, 결과값에 공통된 특정 클래스_KeyClass가 부여된 상태입니다.
+같은 계산기 박스 부모를 가지는 연산기호, 화살표버튼, 결과값, 에러메세지에 공통된 특정 클래스_KeyClass가 부여된 상태입니다.
 (ex- 더하기 박스 안에선 공통 클래스 js-add 부여)
   _KeyClass.classList[0] >> 연산기호
   _KeyClass.classList[1] >> 화살표버튼
   _KeyClass.classList[2] >> 결과값
+  _KeyClass.classList[3] >> 에러 메세지
 클릭 이벤트에 반응하는 화살표 버튼의 KeyClass 를 바탕으로 올바른 계산식의 결과를 정확한 위치에 출력합니다.
 -------------------------------------------
-given a specific class_KeyClass that is common to computational symbols, arrow buttons, and results with the same calculator box parents.
+given a specific class_KeyClass that is common to computational symbols, arrow buttons, results, error messages with the same calculator box parents.
 (ex- 'addition-box' has KeyClass 'js-add')
   _KeyClass.classList[0] >> computational symbol
   _KeyClass.classList[1] >> arrow button
-  _KeyClass.classList[2] >> Result Value
+  _KeyClass.classList[2] >> result value
+  _KeyClass.classList[3] >> error message
 print the results of the correct equation in the correct position based on KeyClass in the arrow buttons that respond to the click event.
 */
 
@@ -20,32 +22,33 @@ const calculate = {
   add: {
     symbol: "+",
     calculus: function () {
-      return firstNum + secondNum;
+      return currentFirstNum + currentSecondNum;
     },
   },
   sub: {
     symbol: "-",
     calculus: function () {
-      return firstNum - secondNum;
+      return currentFirstNum - currentSecondNum;
     },
   },
   mul: {
     symbol: "X",
     calculus: function () {
-      return firstNum * secondNum;
+      return currentFirstNum * currentSecondNum;
     },
   },
   div: {
     symbol: "/",
     calculus: function () {
-      return firstNum / secondNum;
+      return currentFirstNum / currentSecondNum;
     },
   },
 };
 const operators = Object.keys(calculate);
-let firstNum = 0;
-let secondNum = 0;
+let currentKeyClass = "";
 let currentOperator = "";
+let currentFirstNum = 0;
+let currentSecondNum = 0;
 
 function getResult(currentOperator, currentKeyClass) {
   let resultBox = document.getElementsByClassName(currentKeyClass)[2];
@@ -63,9 +66,12 @@ function setEquation(currentKeyClass) {
   )[0];
 
   currentOperator = currentOperatorWrapper.textContent;
-  firstNum = parseInt(currentOperatorWrapper.previousElementSibling.value);
-  secondNum = parseInt(currentOperatorWrapper.nextElementSibling.value);
+  currentFirstNum = parseInt(
+    currentOperatorWrapper.previousElementSibling.value
+  );
+  currentSecondNum = parseInt(currentOperatorWrapper.nextElementSibling.value);
 
+  // isNum();
   getResult(currentOperator, currentKeyClass);
 }
 
@@ -77,7 +83,7 @@ function findKeyClass(e) {
 }
 
 // function isNum(/* object */) {
-//   if (/* !firstNum && !secondNum */ ){
+//   if (/* !currentFirstNum && !currentSecondNum */ ){
 //     object.errorMessage.textContent ="Please enter the empty value";
 //   }
 // }
