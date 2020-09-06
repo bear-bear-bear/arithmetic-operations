3494073bea9694dd6e28377f8ff377c832e2789c
bearbearbear <ghlwkdthfl@gmail.com>
2020-09-07 00:17:37 +0900

    refactor: 변수명을 가독성이 좋게 변경

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// html 상에서 같은 계산기 박스 부모를 가지는 연산기호, 화살표버튼, 결과값에 공통된 클래스_KeyClass 를 부여함
+//  (ex- 더하기 박스 안에선 공통 클래스 js-add 부여)
+// 클릭 이벤트에 반응하는 화살표 버튼의 KeyClass 를 바탕으로 올바른 계산식의 결과를 정확한 위치에 출력
+
 const btn = document.querySelectorAll(".js-result-pointer_btn");
 const calculate = {
   add: {
@@ -32,6 +36,26 @@ const calculate = {
 // const operators = Object.keys(calculate);
 let firstValue = 0;
 let secondValue = 0;
+let currentKeyClass = "";
+
+// function setCalculus() {
+//   console.log(calculate.add.className);
+// }
+
+function setNumbers() {
+  let currentOperator = document.getElementsByClassName(currentKeyClass);
+  firstValue = currentOperator[0].previousElementSibling.value;
+  secondValue = currentOperator[0].nextElementSibling.value;
+  console.log(`firstValue is ${firstValue}, secondValue is ${secondValue}`);
+}
+
+function findKeyClass(e) {
+  const currentBtnClassList = e.target.classList;
+  currentKeyClass = currentBtnClassList[currentBtnClassList.length - 1]; // 클릭된 버튼의 클래스 배열중에 마지막 요소가 연산자를 나타내는 클래스
+  setNumbers();
+  setCalculus();
+  console.log(`currenKeyClass is ${currentKeyClass}`);
+}
 
 // function isNum(/* object */) {
 //   if (/* !firstValue && !secondValue */ ){
@@ -39,23 +63,10 @@ let secondValue = 0;
 //   }
 // }
 
-function setNumbers(currentOperatorClassName) {
-  let currentOperator = document.getElementsByClassName(
-    currentOperatorClassName
-  );
-  firstValue = currentOperator[0].previousElementSibling.value;
-  secondValue = currentOperator[0].nextElementSibling.value;
-}
-
-function findOperator(e) {
-  const currentBtnClassList = e.target.classList;
-  let currentOperatorClassName =
-    currentBtnClassList[currentBtnClassList.length - 1]; // 클릭된 버튼의 클래스 배열중에 마지막 요소가 연산자를 나타내는 클래스
-  setNumbers(currentOperatorClassName);
-}
-
 function init() {
-  btn.forEach(() => addEventListener("click", findOperator));
+  btn.forEach((button) =>
+    button.addEventListener("click", findKeyClass, false)
+  );
 }
 
 init();
